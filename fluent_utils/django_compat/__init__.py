7392d338905904040ec8a6d986d575f418635c7a
Diederik van der Boor <vdboor@edoburu.nl>
2014-09-27 19:24:06 +0200

    add Django 1.7 utils to loop over INSTALLED_APPS

--- fluent_utils/django_compat/__init__.py
+++ fluent_utils/django_compat/__init__.py
@@ -1,7 +1,7 @@
 from .django14 import now, utc, patterns, url, include
 from .django15 import get_user_model, AUTH_USER_MODEL
 from .django16 import transaction_atomic, add_preserved_filters
-from .django17 import is_installed
+from .django17 import is_installed, get_models, get_app_names
 from .python3 import smart_text
 
 __all__ = (
@@ -10,6 +10,6 @@ __all__ = (
     'get_user_model', 'AUTH_USER_MODEL',
     'transaction_atomic',
     'add_preserved_filters',
-    'is_installed',
+    'is_installed', 'get_models', 'get_app_names'
     'smart_text',
 )
--- fluent_utils/django_compat/django17.py
+++ fluent_utils/django_compat/django17.py
@@ -6,6 +6,17 @@ except ImportError:
     # Django 1.6 or below.
     def is_installed(appname):
         return appname in settings.INSTALLED_APPS
+
+    from django.db.models import get_models
+
+    def get_app_names():
+        return settings.INSTALLED_APPS
 else:
     # Django 1.7 provides an official API, and INSTALLED_APPS may contain non-string values too.
     is_installed = apps.is_installed
+    get_models = apps.get_models
+
+    def get_app_names():
+        return [
+            appconfig.name for appconfig in apps.get_app_configs()
+        ]
