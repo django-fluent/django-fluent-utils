51cf0ac826db5a6d1c35836d4a424d31bea2a4ed
Diederik van der Boor <vdboor@edoburu.nl>
2014-09-27 23:01:43 +0200

    Add similar_objects() to TagsMixin

--- fluent_utils/softdeps/taggit.py
+++ fluent_utils/softdeps/taggit.py
@@ -1,6 +1,7 @@
 """
 Optional integration with django-taggit.
 """
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from fluent_utils.django_compat import is_installed
 
@@ -45,3 +46,71 @@ class TagsMixin(models.Model):
 
     class Meta:
         abstract = True
+
+
+    def similar_objects(self, num=None, **filters):
+        """
+        Find similar objects using related tags.
+        """
+        tags = self.tags
+        if not tags:
+            return []
+
+        content_type = ContentType.objects.get_for_model(self.__class__)
+        filters['content_type'] = content_type
+
+        # can't filter, see
+        # - https://github.com/alex/django-taggit/issues/32
+        # - http://django-taggit.readthedocs.org/en/latest/api.html#TaggableManager.similar_objects
+        #
+        # Otherwise this would be possible:
+        # return tags.similar_objects(**filters)
+
+        lookup_kwargs = tags._lookup_kwargs()
+        lookup_keys = sorted(lookup_kwargs)
+        qs = tags.through.objects.values(*lookup_kwargs.keys())
+        qs = qs.annotate(n=models.Count('pk'))
+        qs = qs.exclude(**lookup_kwargs)
+        subq = tags.all()
+        qs = qs.filter(tag__in=list(subq))
+        qs = qs.order_by('-n')
+
+        # from https://github.com/alex/django-taggit/issues/32#issuecomment-1002491
+        if filters is not None:
+            qs = qs.filter(**filters)
+
+        if num is not None:
+            qs = qs[:num]
+
+        # Normal taggit code continues
+
+        # TODO: This all feels like a bit of a hack.
+        items = {}
+        if len(lookup_keys) == 1:
+            # Can we do this without a second query by using a select_related()
+            # somehow?
+            f = tags.through._meta.get_field_by_name(lookup_keys[0])[0]
+            objs = f.rel.to._default_manager.filter(**{
+                "%s__in" % f.rel.field_name: [r["content_object"] for r in qs]
+            })
+            for obj in objs:
+                items[(getattr(obj, f.rel.field_name),)] = obj
+        else:
+            preload = {}
+            for result in qs:
+                preload.setdefault(result['content_type'], set())
+                preload[result["content_type"]].add(result["object_id"])
+
+            for ct, obj_ids in preload.items():
+                ct = ContentType.objects.get_for_id(ct)
+                for obj in ct.model_class()._default_manager.filter(pk__in=obj_ids):
+                    items[(ct.pk, obj.pk)] = obj
+
+        results = []
+        for result in qs:
+            obj = items[
+                tuple(result[k] for k in lookup_keys)
+            ]
+            obj.similar_tags = result["n"]
+            results.append(obj)
+        return results
